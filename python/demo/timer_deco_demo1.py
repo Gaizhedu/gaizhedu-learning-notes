653d40d61491c158d3b34919c276bbb3586965c9
Gaizhedu <Crystallize_silver@outlook.com>
2025-07-29 10:33:13 +0800

    新增计时器装饰器例1

---
+++ python/demo/timer_deco_demo1.py
@@ -0,0 +1,23 @@
+import time
+
+
+def timer(func):
+    def check(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"函数{func.__name__}的执行时间为：{end_time - start_time:.4f}s")
+        return result
+
+    return check
+
+
+@timer
+def test1(a, b):
+    time.sleep(0.5)  # 模拟耗时操作
+    return a + b
+
+
+test1(3,5)
+
+# 输出：函数test1的执行时间为：0.5004s
